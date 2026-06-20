a00bdf49294986403c13d3d67ea6b4ba0bcd2a2c
Uzair Ali <uzaairaamir@gmail.com>
2026-06-20 11:10:34 -0700

    Refactor app.js to include server start logic

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,11 @@ const apiRoutes = require("./routes/apiRoutes");
 dotenv.config();
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+
 
 app.use(cors());
-
-app.options("*", cors(corsOptions));
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
@@ -30,9 +29,11 @@ app.use((err, req, res, next) => {
   });
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
-
+// For local development
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+}
 
 module.exports = app;
